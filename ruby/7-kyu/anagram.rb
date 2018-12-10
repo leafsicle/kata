54e6ea95a50cd545c3a0b9281d44dd8efedcde7b
Matthew Cooke <cookem529@gmail.com>
2018-12-10 17:53:02 -0500

    added clever solution

--- ruby/7-kyu/anagram.rb
+++ ruby/7-kyu/anagram.rb
@@ -1,4 +1,7 @@
 def is_anagram(test, original)
   (test.chars.map(&:downcase).sort)==
   (original.chars.map(&:downcase).sort)
+  # smarter solution 
+  # test.downcase.chars.sort == original.downcase.chars.sort
+
 end
