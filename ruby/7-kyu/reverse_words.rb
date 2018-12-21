b695a7d338534f6ecdc283b4924f0681790d39df
Matthew Cooke <cookem529@gmail.com>
2018-12-21 12:06:33 -0500

    new comments

--- ruby/7-kyu/reverse_words.rb
+++ ruby/7-kyu/reverse_words.rb
@@ -2,3 +2,5 @@ def reverse_words(str)
   str.split(/ /).map {|x| x.reverse}.join(" ")
 end
 #I learned a thing!
+#   str.gsub(/\S+/, &:reverse)
+# this was a better way to do this. I need to learn gsub better
