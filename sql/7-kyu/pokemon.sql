be343c60fae56e746c1ee54a6c7da46289caef7a
Matt C <cookem529@gmail.com>
2021-08-15 12:14:24 -0400

    too eager to submit sql. needed to rename columname

--- sql/7-kyu/pokemon.sql
+++ sql/7-kyu/pokemon.sql
@@ -1,5 +1,5 @@
-select pkm.pokemon_name,pkm.str, mult.element from pokemon pkm
-join multipliers mult on pkm.element_id= mult.id
-Where str >=40
-order by str asc
-
+select pkm.pokemon_name, pkm.str*mult.multiplier as modifiedStrength, mult.element
+from pokemon pkm join multipliers mult
+on pkm.element_id= mult.id
+Where pkm.str*mult.multiplier >=40
+order by 2 desc
