370534b5c79308d156b34b2c936f9858cc33ace8
Matt C <cookem529@gmail.com>
2021-08-20 13:25:06 -0400

    There we go.
    forwards and backawards

--- js/6-kyu/longPalondrome.js
+++ js/6-kyu/longPalondrome.js
@@ -1,32 +1,9 @@
-is_Palindrome= str=> {
-  let rev = str.split("").reverse().join("");
-  return str == rev;
-}
-
-longestPalindrome=(str)=>{
-  if (is_Palindrome(str)){
-    return str.length
-  } 
-  
-  let max_length = 0, maxp = ''
-
-  for(let i=0; i < str.length; i++) {
-    let subs = str.substr(i, str.length)
-
-    for(let j=subs.length; j>=0; j--){
-      let sub_subs_str = subs.substr(0, j)
-      if (sub_subs_str.length <= 1)
-        continue
-
-      if (is_Palindrome(sub_subs_str)){
-        if (sub_subs_str.length > max_length) {
-          max_length = sub_subs_str.length;
-          maxp = sub_subs_str;
-        }
-      }
+longestPalindrome= s=>{
+  if (!s) return 0
+  for (let i = s.length; i > 0; i--) {
+    for (let j = 0; j <= s.length - i; j++) {
+      var check = s.substr(j, i)
+      if (check === check.split("").reverse().join("")) return i
     }
   }
-  console.log(str,maxp.length)
-return maxp.length
-  
 }
